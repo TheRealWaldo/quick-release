f01b88ed49df1d2d6f891fd270037e9a52de908a
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-11-03 15:16:05 -0600

    ci: update to use true-conventional-commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  parserPreset: {
-    parserOpts: {
-      issuePrefixes: ['RIT-'],
-    },
-  },
-  rules: {
-    'references-empty': [1, 'never'],
-    'body-max-line-length': [0, 'always', 'Infinity'],
-  },
+  extends: ['true-conventional-commits'],
 };
