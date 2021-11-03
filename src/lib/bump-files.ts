f8e150fca69415d6a9fd86ae3a5b5d1cdd472b1c
Will Freeman <6641284+TheRealWaldo@users.noreply.github.com>
2021-11-03 09:38:43 -0600

    feat: run npm version when package.json in replace-files

--- src/lib/bump-files.ts
+++ src/lib/bump-files.ts
@@ -1,5 +1,6 @@
 import { replaceInFile } from 'replace-in-file';
 import { debug } from '@actions/core';
+import { npmVersion } from './npm-operations';
 
 export async function bumpFiles(
   latestVersion: string,
@@ -8,11 +9,18 @@ export async function bumpFiles(
 ) {
   if (files && files.length) {
     debug(`Bumping version in ${files.length} files...`);
-    return replaceInFile({
-      files: files,
-      from: new RegExp(latestVersion, 'g'),
-      to: recommendedVersion,
-    });
+    if (files.includes('package.json')) {
+      npmVersion(recommendedVersion);
+    }
+    const straightReplaceFiles = files.filter((file) => file !== 'package.json');
+    if (straightReplaceFiles && straightReplaceFiles.length) {
+      return replaceInFile({
+        files: straightReplaceFiles,
+        from: new RegExp(latestVersion, 'g'),
+        to: recommendedVersion,
+      });
+    }
+    return {};
   } else {
     debug(`No files to bump.`);
     return {};
